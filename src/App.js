a9ab98f50a56d17aafe669bf8961dcbba4db0092
GurTati <tatiana.gurova.j@gmail.com>
2023-05-10 14:02:22 +0100

    create a react app for quiz

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Team Introduction</h1>
+
+        <div>
+          <h2>Tatiana Gurova</h2>       
+            <p>I like hiking and I love to sew.</p>
+            <p>Because I like active rest and I like to create something with my hands.</p>
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
