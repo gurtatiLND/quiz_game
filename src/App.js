fe867b5715ba12c12bbdc2e08717817ad7fb18e5
GurTati <tatiana.gurova.j@gmail.com>
2023-05-10 15:25:13 +0100

    add my details to the app.js(Tatiana G.)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Team Introduction</h1>
+        <div>
+          <h3>Tatiana Gurova</h3>
+            <p>I like hiking.</p>
+            <p>Because I love nature and outdoor activities.</p>
+        </div>
       </header>
     </div>
   );
