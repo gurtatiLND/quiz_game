4a2209332ea12a22165a18c8bf34596534bee951
TATIANA GUROVA <118075413+gurtatiLND@users.noreply.github.com>
2023-05-10 15:08:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Team Introduction</h1>
-
-        <div>
-          <h2>Tatiana Gurova</h2>       
-            <p>I like hiking and I love to sew.</p>
-            <p>Because I like active rest and I like to create something with my hands.</p>
-        </div>
-        
       </header>
     </div>
   );
