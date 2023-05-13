01ab94a267d84ac14a79fa055cf528f7709965b8
GurTati <tatiana.gurova.j@gmail.com>
2023-05-13 14:30:00 +0100

    create Questions component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 import Questions from './Questions';
 
 function App() {
-  const URL = 'https://opentdb.com/api.php?amount=10'
-  const [data, setData] = useState("test")
-
-  useEffect(() => {
-    fetch(URL)
-    .then(res => {
-      return res.json();
-    })
-    .then(data => {
-      setData(data)
-    })
-  }, [])
-
-  const getQuestions = () => {
-    return data.results.map(item => (
-      <li>{item.question}</li>
-    ))
-  }
-
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <div>{console.log(data.results)}</div>
-        {/* <div>
-          {dataTest.map(e => (
-            <p>{e.name}</p>
-          ))}
-        </div> */}
-        <div>
-          {data.results.map(item => (
-            <p>{item.question}</p>
-          ))}
-        </div>
-  
-        <Questions questions={getQuestions()} />
-      </header>
+      <div className='Content'>
+        <Questions />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+{/* I left in APP.js only the link to the component "questions" 
+and  transferred all the mechanics in this component */}
--- src/Question.js
+++ src/Question.js
@@ -1,9 +1,17 @@
-import React from 'react'
+{/* this component is absolutely "empty". 
+I've just created it but didn't think about logic.
+Please, keep going if you have an idea how to do it. */}
 
-function Question({e}) {
+{/* I thought to use props from "Questions", but this is raw idea. */}
+function Question({ quizData, title }) {
+
   return (
-    <div>{e}</div>
+     <div className="Questions-list">
+      <h2>{ title }</h2>
+      { quizData }
+
+     </div>
   )
 }
 
-export default Question
+export default Question;
