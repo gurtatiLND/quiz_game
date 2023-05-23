980549c7b323f7928d97285a1c96c79de6f8eb7f
Giny Huynh <ginyhuynh@gmail.com>
2023-05-24 00:43:33 +0100

    started reviewpage styling

--- src/Components/ReviewAnswers.js
+++ src/Components/ReviewAnswers.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import _ from 'lodash';
 import {convertToNormalString} from './convertToNormalString';
+import "./ReviewAnswers.css";
 
 //still in progress
 //now is just showing all answers
@@ -12,7 +13,7 @@ const ReviewAnswers = ({answers, wrongAnswersAndQuestions,}) => {
                 return <div key={index}>
                     <div className='question'>
                         <h3>Question {question.number}</h3>
-                        {convertToNormalString(question.question)}
+                        <p>{convertToNormalString(question.question)}</p>
                     </div>
                     <div className='answers'>
                         {_.shuffle([
@@ -25,15 +26,37 @@ const ReviewAnswers = ({answers, wrongAnswersAndQuestions,}) => {
                             const rightAnswer = answer === question.good_answer
                                 ? 'correct_answer'
                                 : null;
-                            return <div key={index} className={_.compact([userAnswer, rightAnswer]).join(' ')}>
+
+                            let wrongans=convertToNormalString(answers[question.number - 1]);
+                            console.log(wrongans);
+                            let rightans=convertToNormalString(question.good_answer);
+                            console.log(rightans);
+                            let otherans = question.bad_answers;
+                            let indexofwrong = otherans.indexOf(wrongans);
+                            delete otherans[indexofwrong];
+                            let otherans2 = otherans.filter( String);
+                            console.log(otherans2);
+
+                            return (
+                                <div>
+                                    <p className = "wrongAnswer">{wrongans}</p>
+                                    <p className = "rightAnswer">{rightans}</p>
+                                    <div key={index}>
+                                        <div><p>{otherans2}</p></div>
+                                    </div>
+                                </div>
+                                
+                            )
+
+/*                             return <div key={index} className={_.compact([userAnswer, rightAnswer]).join(' ')}>
                                 <div>{convertToNormalString(answer)}</div>
-                            </div>
+                            </div>  */
                         })}
                     </div>
                 </div>;
             })}
 
-            
+            <p/>
             <button >
                 <Link to='/'>Play again</Link>
             </button>
