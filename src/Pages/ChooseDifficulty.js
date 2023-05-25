3667ce96ac9bc388f1349426fe4d7c225a01528a
clarice_r <claricerdallorto@gmail.com>
2023-05-25 21:28:59 +0100

    Refactor choose difficulty to use useNavigate instead of Link

--- src/Pages/ChooseDifficulty.js
+++ src/Pages/ChooseDifficulty.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import LogoHeader from '../logos/LogoHeader'
 
 function ChooseDifficulty() {
     const location = useLocation()
-    const {category} = location.state
+    const category = location.state
+    const navigate = useNavigate()
+    const [difficulty, setDifficulty] = useState('easy')
+
+
+    const handleChooseDifficulty = (e) => {
+        const option = e.target.innerText
+        switch (option) {
+            case 'Easy':
+                setDifficulty('easy')
+                break
+            case 'Medium':
+                setDifficulty('medium')
+                break
+            case 'Advanced':
+                setDifficulty('hard')
+                break
+            default:
+                return difficulty
+        }
+        navigate('quiz', {state: {category: category, difficulty: difficulty}})
+    }
   return (
     <>
     <LogoHeader/>
@@ -12,16 +33,16 @@ function ChooseDifficulty() {
 
         <h3>Choose Difficulty</h3>
         <div>
-            <button>
-                <Link to='quiz' state={{ difficulty: 'easy', category: category }}>Easy</Link>
+            <button onClick={handleChooseDifficulty}>Easy
+                {/* <Link to='quiz' state={{ difficulty: 'easy', category: category }}>Easy</Link> */}
             </button>
             <p/>
-            <button>
-                <Link to='quiz' state={{ difficulty: 'medium', category: category }}>Medium</Link>
+            <button onClick={handleChooseDifficulty}>Medium
+                {/* <Link to='quiz' state={{ difficulty: 'medium', category: category }}>Medium</Link> */}
             </button>
             <p/>
-            <button>
-                <Link to='quiz' state={{ difficulty: 'hard', category: category }}>Advanced</Link>
+            <button onClick={handleChooseDifficulty}>Advanced
+                {/* <Link to='quiz' state={{ difficulty: 'hard', category: category }}>Advanced</Link> */}
             </button>             
                
         </div>
