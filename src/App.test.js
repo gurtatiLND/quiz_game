8502ead0e837586d1666e143cc573dc8f98a1a24
Giny Huynh <ginyhuynh@gmail.com>
2023-05-27 14:20:21 +0100

    added 1 test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react'
+import { render, screen, fireEvent, cleanup } from '@testing-library/react'
 import StartPage from './Pages/StartPage'
-import { MemoryRouter } from 'react-router-dom';
+import ChooseCategory from './Pages/ChooseCategory'
+import { categories } from './Utils/categories'
+import { MemoryRouter } from 'react-router-dom'
 
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...(jest.requireActual("react-router-dom")),
+  useNavigate: () => mockedUsedNavigate
+}));
 
 test('renders StartPage component', () => {
   render(
@@ -25,3 +32,27 @@ test('input field updates the name state', () => {
   expect(nameInput.value).toBe('John Doe');
 });
 
+test('start button takes user to the ChooseCategory page', () => {
+  render(
+    <MemoryRouter>
+      <StartPage />
+    </MemoryRouter>
+  );
+  const startButton = screen.getByRole('button', { name: /start/i });
+  fireEvent.click(startButton);
+  expect(mockedUsedNavigate).toHaveBeenCalledWith("category", {"state": "Player"});
+});
+
+/* test('Choosing a category (General) takes user to the ChooseDifficulty page', () => {
+  const mockedHandleChooseCategory = jest.fn()
+  render(
+    <MemoryRouter>
+      <ChooseCategory handleChooseCategory={mockedHandleChooseCategory}/>
+    </MemoryRouter>
+  );
+  const generalCategoryButton = screen.queryByTestId("generalCategoryButton");
+  fireEvent.click(generalCategoryButton);
+  //expect(categories["general"]).toBe(9);
+  expect(mockedHandleChooseCategory).toHaveBeenCalled();
+  expect(mockedUsedNavigate).toHaveBeenCalledWith('difficulty', { state: generalCategory });
+}); */
